baf430fa16a89beb097d2e0a5a416fb824df033f
TheKosky02 <82919229+thekosky02@users.noreply.github.com>
2022-09-07 18:52:16 -0500

    front-end MascotaFeliz

---
+++ MascotaFeliz.App.Persistencia/Migrations/20220906030953_segunda.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MascotaFeliz.App.Persistencia.Migrations
+{
+    public partial class segunda : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "cedula",
+                table: "Personas",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "cedula",
+                table: "Personas");
+        }
+    }
+}
