1ad1cd61fdf675fcbaf370ed80714e7fde78d4f6
jcariasmoque <98795363+jcariasmoque@users.noreply.github.com>
2022-09-15 17:56:34 -0500

    ad editardueno

--- MascotaFeliz.App/MascotaFeliz.App.Frontend/Pages/Duenos/ListaDuenos.cshtml.cs
+++ MascotaFeliz.App/MascotaFeliz.App.Frontend/Pages/Duenos/ListaDuenos.cshtml.cs
@@ -11,8 +11,15 @@ namespace MascotaFeliz.App.Frontend.Pages
 {
     public class ListaDuenosModel : PageModel
     {
-         private static IRepositorioDueno _repoDueno = new RepositorioDueno(new Persistencia.AppContext());
-         public IEnumerable<Dueno> listaDuenos {get;set;}
+        private readonly IRepositorioDueno _repoDueno;
+        public IEnumerable<Dueno> listaDuenos {get;set;}
+
+        public ListaDuenosModel()
+        {
+            this._repoDueno = new RepositorioDueno(new Persistencia.AppContext());
+        }
+         
+         
         public void OnGet()
         {
             listaDuenos = _repoDueno.GetAllDuenos();
